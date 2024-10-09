b00e8f1a6db222d54509bb2790e4688c6888eea8
Shashwat Srivastava <85879283+ShashwatSrivastava07@users.noreply.github.com>
2024-10-09 16:07:40 +0530

    Update Herosection.tsx

--- src/Components/Herosection.tsx
+++ src/Components/Herosection.tsx
@@ -26,7 +26,7 @@ export function HeroSection() {
         <div className="flex flex-col justify-center items-center">
           <div className="h-[40rem] sm:h-[30rem] md:h-[50rem] flex-grow-0 flex justify-center items-center px-2">
             <div className="text-4xl md:text-5xl mx-auto font-normal text-neutral-600 dark:text-neutral-400 leading-snug">
-              I&#39;m
+              I&#39;m <br></br>
               <FlipWords words={words} /> <br />
               dedicated to crafting engaging web experiences.
             </div>
