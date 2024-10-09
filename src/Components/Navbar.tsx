794b19ef7cdb4095a125f38e032aabc0a9477189
Shashwat Srivastava <85879283+ShashwatSrivastava07@users.noreply.github.com>
2024-10-09 12:39:13 +0530

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { FloatingNav } from "@/Components/ui/floating-navbar";
-import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { IconHome, IconUser } from "@tabler/icons-react";
 
 export function NavbarMain() {
     const navItems = [
