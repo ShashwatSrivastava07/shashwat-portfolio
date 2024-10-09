4adbffbe73cdebaf7cea7d896228007c53b79585
Shashwat Srivastava <85879283+ShashwatSrivastava07@users.noreply.github.com>
2024-10-09 15:33:43 +0530

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { FloatingNav } from "@/Components/ui/floating-navbar";
-import { IconHome, IconUser } from "@tabler/icons-react";
+import { IconHome, IconUser, IconCode,IconBriefcaseFilled,IconApps } from "@tabler/icons-react";
 
 export function NavbarMain() {
     const navItems = [
@@ -18,14 +18,17 @@ export function NavbarMain() {
         {
             name: "Skills",
             link: "#skills-section",
+            icon: <IconCode className="h-4 w-4 text-white" />,
         },
         {
             name: "Experience",
             link: "#exp-section",
+            icon: <IconBriefcaseFilled className="h-4 w-4 text-white" />,
         },
         {
             name: "Project",
             link: "#projects-section",
+            icon: <IconApps className="h-4 w-4 text-white" />,
         },
     ];
     return (
